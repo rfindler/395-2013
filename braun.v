8c2f91c234060427e521588c5afdcf22d9656fdc
Robby Findler <robby@racket-lang.org>
2013-11-19 22:07:49 -0600

    adjustments today in class

--- braun.v
+++ braun.v
@@ -1,6 +1,6 @@
 Require Import Arith List JMeq Omega.
 Require Import Arith.Even Arith.Div2.
-Require Import CpdtTactics.
+Require Import CpdtTactics Coq.Program.Wf.
 
 Set Implicit Arguments.
 
@@ -19,8 +19,8 @@ Inductive same_structure :
   forall (n n' : nat),
     braun_tree n -> braun_tree n' -> Prop :=
 | SEmpty : same_structure Empty Empty
-| SNode : forall (x1 x2:A) 
-                 (nl1 nl2 nr1 nr2: nat)
+| SNode : forall (x1 x2 : A) 
+                 (nl1 nl2 nr1 nr2 : nat)
                  (h1 : nr1 <= nl1 <= nr1 + 1)
                  (h2 : nr2 <= nl2 <= nr2 + 1)
                  (l1 : braun_tree nl1)
@@ -31,6 +31,13 @@ Inductive same_structure :
             same_structure r1 r2 ->
             same_structure (Node x1 h1 l1 r1)
                            (Node x2 h2 l2 r2).
+
+Theorem same_structure_same_size : 
+  forall n1 n2 (b1 : braun_tree n1) (b2 : braun_tree n2),
+    same_structure b1 b2 -> n1 = n2.
+  intros n1 n2 b1 b2.
+  induction 1; subst; reflexivity.
+Qed.
 
 Theorem same_size_same_structure :
   forall n (b1 b2 : braun_tree n), 
@@ -61,10 +68,13 @@ End braun_tree.
 Implicit Arguments Empty [A].
 Implicit Arguments Node [A].
 
-Program Fixpoint insert A n (x:A) (b : braun_tree A n) : braun_tree A (n+1) :=
+Program Fixpoint insert A n (x:A)
+        (b : braun_tree A n)
+: braun_tree A (n+1) :=
   match b with
     | Empty => Node x 0 0 _ Empty Empty
-    | Node y s_size t_size p s t => Node x (t_size+1) s_size _ (insert y t) s
+    | Node y s_size t_size p s t => 
+      Node x (t_size+1) s_size _ (insert y t) s
   end.
 
 Obligation 2. omega. Qed.

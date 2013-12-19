b7c45d53da223f3417dbba5ece0c5213ac8ebe0b
Max New <maxsnew@gmail.com>
2013-12-19 16:13:50 -0600

    Add naive make array running time proof (by construction).
    It's probably quite a bit more effort to show that the rt function is
    O(nlog(n)).

---
+++ counting_make_array.v
@@ -0,0 +1,45 @@
+Require Import braun util monad2 fl_log insert_time_by_construction.
+Require Import Div2 List.
+Set Implicit Arguments.
+
+Section ilist.
+  Variable A : Set.
+  Variable P : nat -> Set.
+
+  Inductive ilist : nat -> Set :=
+  | Nil  : ilist 0
+  | Cons : forall n, A -> ilist n -> ilist (S n).
+End ilist.
+
+Section ifoldr.
+  Variables A : Set.
+  Variable B : nat -> Set.
+  Variable f : forall n, A -> B n -> B (S n).
+  Variable i : B 0.
+
+  Fixpoint ifoldr n (ls : ilist A n) : B n :=
+    match ls with
+      | Nil => i
+      | Cons _ x ls' => f x (ifoldr ls')
+    end.
+End ifoldr.
+
+Section make_array_naive.
+  Variable A : Set.
+
+  (* Not sure how to prove this is O(nlogn) *)
+  Fixpoint rt_naive n : nat :=
+    match n with
+      | 0 => 0
+      | S n' => rt_naive n' + (fl_log n' + 1)
+    end.
+
+  Program Definition make_array_naive n (s : ilist A n)
+  : C (braun_tree A n) (rt_naive n) :=
+    ifoldr (fun n => C (braun_tree A n) (rt_naive n))
+           (fun n x ct => ct >>= insert x)
+           (ret Empty)
+           s.
+  Obligation 1. omega. Qed.
+
+End make_array_naive.

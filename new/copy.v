740a8c0a21cf0224d172c174f16b407a14ef20a6
Jay McCarthy <jay@racket-lang.org>
2013-12-25 15:30:20 -0600

    improve extraction

--- new/copy.v
+++ new/copy.v
@@ -7,7 +7,8 @@ Section copy.
   Variable x : A.
 
   Inductive Copy2R : nat -> (bin_tree A) * (bin_tree A) -> nat -> Prop :=
-    C2R_zero : Copy2R 0 (bt_mt A, bt_node x (bt_mt A) (bt_mt A)) 1
+    C2R_zero : 
+      Copy2R 0 (bt_mt A, bt_node x (bt_mt A) (bt_mt A)) 1
   | C2R_even : 
       forall n' time s t, 
         even n' ->
@@ -18,17 +19,19 @@ Section copy.
         odd n' ->
         Copy2R (div2 n') (s,t) time ->
         Copy2R (S n') (bt_node x s s, bt_node x s t) (time+2).
+  Hint Constructors Copy2R.
 
   Inductive CopyR : nat -> bin_tree A -> nat -> Prop :=
-    CR : forall n bt1 bt2 time,
-           Copy2R n (bt1,bt2) time ->
-           CopyR n bt1 time.
-
-  Hint Constructors Copy2R.
+    CR : 
+      forall n bt1 bt2 time,
+        Copy2R n (bt1,bt2) time ->
+        CopyR n bt1 time.
   Hint Constructors CopyR.
 
   Theorem copy2 : 
-    forall (n:nat), {pr | exists time, Copy2R n pr time}.
+    forall (n:nat),
+      {pr | exists time, Copy2R n pr time}.
+  Proof.
     apply (well_founded_induction_type
              lt_wf
              (fun n => {pr | exists time, Copy2R n pr time})).
@@ -40,7 +43,7 @@ Section copy.
     clear IND; destruct PR as [s t].
     destruct (even_odd_dec n).
 
-    exists (bt_node x s t,bt_node x t t).
+    exists (bt_node x s t, bt_node x t t).
     destruct IND2.
     eauto.
 
@@ -51,6 +54,7 @@ Section copy.
 
   Theorem copy : 
     forall (n:nat), {bt | exists time, CopyR n bt time}.
+  Proof.
     intros n.
     destruct (copy2 n) as [[s t] E].
     exists s.
--- new/extract.v
+++ new/extract.v
@@ -1,3 +1,12 @@
-Require Import insert copy.
+Require Import insert copy Arith.Div2 Arith.Even.
 
-Recursive Extraction insert.insert copy.copy.
+Extract Inductive bool => "bool" [ "false" "true" ].
+Extract Inductive sumbool => "bool" [ "false" "true" ].
+Extract Inductive prod => "(*)"  [ "(,)" ].
+Extract Inductive nat => 
+"int" [ "0" "succ" ]
+      "(fun fO fS n -> if n=0 then fO () else fS (n-1))".
+Extract Constant div2 => "fun a -> a / 2".
+Extract Constant even_odd_dec => "fun a -> (a mod 2) != 0".
+
+Extraction "braun.ml" insert.insert copy.copy.

cb3fff182ed738c81852cf27412448cb248e2afb
Robby Findler <robby@racket-lang.org>
2013-12-26 00:12:05 -0600

    fixed up problems with copy and proved copy2 result
    ... but broke extraction along the way.

--- new/copy.v
+++ new/copy.v
@@ -1,4 +1,5 @@
 Require Import util log braun.
+Require Import Program.Equality.
 Require Import Omega.
 Require Import Arith Arith.Even Arith.Div2.
 
@@ -8,17 +9,15 @@ Section copy.
 
   Inductive Copy2R : nat -> (bin_tree A) * (bin_tree A) -> nat -> Prop :=
     C2R_zero : 
-      Copy2R 0 (bt_mt A, bt_node x (bt_mt A) (bt_mt A)) 1
+      Copy2R 0 (bt_node x (bt_mt A) (bt_mt A),bt_mt A) 1
   | C2R_even : 
-      forall n' time s t, 
-        even n' ->
-        Copy2R (div2 n') (s,t) time ->
-        Copy2R (S n') (bt_node x s t, bt_node x t t) (time+2)
+      forall m time s t, 
+        Copy2R m (s,t) time ->
+        Copy2R (2*m+1) (bt_node x s t, bt_node x t t) (time+2)
   | C2R_odd : 
-      forall n' time s t, 
-        odd n' ->
-        Copy2R (div2 n') (s,t) time ->
-        Copy2R (S n') (bt_node x s s, bt_node x s t) (time+2).
+      forall m time s t, 
+        Copy2R m (s,t) time ->
+        Copy2R (2*m+2) (bt_node x s s, bt_node x s t) (time+2).
   Hint Constructors Copy2R.
 
   Inductive CopyR : nat -> bin_tree A -> nat -> Prop :=
@@ -28,7 +27,8 @@ Section copy.
         CopyR n bt1 time.
   Hint Constructors CopyR.
 
-  Theorem copy2 : 
+(*
+  Lemma copy2 : 
     forall (n:nat),
       {pr | exists time, Copy2R n pr time}.
   Proof.
@@ -61,5 +61,36 @@ Section copy.
     destruct E.
     eauto.
   Defined.
-
+*)
 End copy.
+
+  Lemma Copy2_produces_Braun :
+    forall A x n bt1 bt2 time, 
+      (Copy2R A x n (bt1,bt2) time)
+      -> Braun bt1 (n+1) /\ Braun bt2 n.
+    intros A x n s t time.
+    intros CSR.
+    dependent induction CSR; try (inversion IHCSR; clear IHCSR).
+
+    constructor.
+    replace (0+1) with (0+0+1);[|omega].
+    constructor; [omega|constructor|constructor].
+    constructor.
+
+    constructor.
+
+    replace (2*m+1+1) with ((m+1)+m+1);[|omega].
+    constructor;[omega|assumption|assumption].
+
+    replace (2*m+1) with (m+m+1);[|omega].
+    constructor;[omega|assumption|assumption].
+
+    constructor.
+
+    replace (2*m+2+1) with ((m+1)+(m+1)+1);[|omega].
+    constructor;[omega|assumption|assumption].
+
+    replace (2*m+2) with ((m+1)+m+1);[|omega].
+    constructor;[omega|assumption|assumption].
+Qed.
+

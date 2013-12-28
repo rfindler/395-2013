423125a655ba2b85f191a0174be6087cfdeb3aab
Jay McCarthy <jay.mccarthy@gmail.com>
2013-12-27 17:31:33 -0700

    plus_r_inj doesn't exist on my machine?

--- new/make_array.v
+++ new/make_array.v
@@ -1,10 +1,12 @@
 Require Import braun log insert util.
+Require Import Arith List.
 
 Section naive1.
   Variable A : Set.
 
   Inductive MkArrNaive1R : list A -> bin_tree A -> nat -> Prop :=
-  | naive1R_zero : MkArrNaive1R nil (bt_mt A) 0
+  | naive1R_zero : 
+      MkArrNaive1R nil (bt_mt A) 0
   | naive1R_suc  :
       forall x xs bt bt' time insert_time,
         InsertR A x bt bt' insert_time ->
@@ -15,7 +17,7 @@ Section naive1.
   Theorem make_array_naive1 :
     forall xs,
       { bt | exists n, MkArrNaive1R xs bt n }.
-
+  Proof.
     induction xs as [| x xs]; [eauto |].
     destruct IHxs as [bt IR].
     remember (insert A x bt).
@@ -32,7 +34,7 @@ Section naive1.
     forall xs bt n,
       MkArrNaive1R xs bt n ->
       Braun bt (length xs).
-
+  Proof.
     intro xs.
     induction xs as [| x xs]; intros bt n MkArrR.
 
@@ -53,11 +55,12 @@ Section naive1.
       | S n' => rt_naive n' + (fl_log n' + 1)
     end.
 
-  Require Import List.
   Example rt_naive_ex :
     map rt_naive (1 :: 2 :: 3 :: 4 ::  5 ::  6 ::  7 ::  8 ::  9 :: 10 :: nil)
                = (1 :: 3 :: 5 :: 8 :: 11 :: 14 :: 17 :: 21 :: 25 :: 29 :: nil).
-  compute; reflexivity. Qed.
+  Proof.
+    auto.
+  Qed.
 
   (* Helper *)
   Theorem MkArrNaive1R_time_help :
@@ -79,7 +82,7 @@ Section naive1.
 
     destruct IHIndR as [IHTime IHBraun].
     simpl; subst.
-    apply plus_r_inj.
+    cut (insert_time = (fl_log (length xs) + 1)); try omega.
     apply (InsertR_time A x _ _ bt bt'); auto.
   Qed.
   
@@ -89,6 +92,5 @@ Section naive1.
       n = rt_naive (length xs).
   Proof.
     apply MkArrNaive1R_time_help.
-  Qed.
-    
+  Qed.  
 End naive1.

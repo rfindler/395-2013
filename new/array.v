6df53c98e3ed21434679a93ed02232ab81eaa559
Jay McCarthy <jay.mccarthy@gmail.com>
2013-12-31 22:56:44 -0700

    add sequence

--- new/array.v
+++ new/array.v
@@ -1,4 +1,4 @@
-Require Import braun log insert util index list.
+Require Import braun log insert util index list sequence.
 Require Import Arith Arith.Even Arith.Div2 List.
 Require Import Program.
 Require Import Omega.
@@ -6,27 +6,19 @@ Require Import Omega.
 Section array.
   Variable A : Set.
 
-  Inductive SequenceR : bin_tree A -> list A -> Prop :=
-    | SR_mt :
-        SequenceR (bt_mt A) nil
-    | SR_node :
-        forall x s t ss ts,
-          SequenceR s ss ->
-          SequenceR t ts ->
-          SequenceR (bt_node x s t) (x::interleave A ss ts).
-  Hint Constructors SequenceR.
-
   Lemma InsertR_SequenceR:
     forall t ts y t' n,
-      SequenceR t ts ->
+      SequenceR A t ts ->
       InsertR A y t t' n ->
-      SequenceR t' (y :: ts).
+      SequenceR A t' (y :: ts).
   Proof.
     induction t as [|x s IHs t IHt]; intros ts y t' n SR IR.
 
     invclr SR. invclr IR.
     cut (nil = interleave A nil nil). intros EQ; rewrite EQ.
     eapply SR_node; eauto.
+    apply SR_mt.
+    apply SR_mt.
     auto.
 
     invclr SR. rename H3 into SRs. rename H4 into SRt.
@@ -49,10 +41,11 @@ Section array.
   Theorem MakeArrayLinearR_SequenceR :
     forall xs bt n,
       MakeArrayLinearR xs bt n ->
-      SequenceR bt xs.
+      SequenceR A bt xs.
   Proof.
     intros xs bt n MALR.
     induction MALR; eauto.
+    apply SR_mt.
     eapply InsertR_SequenceR; eauto.
   Qed.
 
@@ -89,69 +82,6 @@ Section array.
     apply (InsertR_Braun A x (length xs) insert_time bt0).
     apply (IHxs bt0 time H0).
     assumption.
-  Qed.
-
-  Lemma BraunR_SequenceR :
-    forall b n,
-      Braun b n ->
-      forall l,
-        SequenceR b l ->
-        n = (length l).
-  Proof.
-    intros b n B.
-    induction B; intros l SR; invclr SR.
-    auto.
-
-    rename H into BP.
-    rename H4 into SRs.
-    rename H5 into SRt.
-    apply IHB1 in SRs.
-    apply IHB2 in SRt.
-    subst.
-    rewrite interleave_length_split.
-    simpl.
-    omega.
-  Qed.
-
-  Theorem SequenceR_IndexR :
-    forall b i x,
-      IndexR A b i x ->
-      forall xs,
-        Braun b (length xs) ->
-        SequenceR b xs ->
-        ListIndexR A xs i x.
-  Proof.
-    intros b i x IR.
-    induction IR; intros xs BP SR; invclr SR; eauto;
-    rename H3 into SRs; rename H4 into SRt.
-
-    eapply LIR_zero.
-
-    invclr BP.
-    rename H3 into BP.
-    rename H4 into Bs.
-    rename H5 into Bt.
-    rename H2 into EQ.
-    rewrite <- interleave_length_split in EQ.
-    replace s_size with (length ss) in *; try omega.
-    replace t_size with (length ts) in *; try omega.
-    apply IHIR in SRs; eauto.
-    apply ListIndexR_interleave_evens; eauto.
-    symmetry. eapply BraunR_SequenceR. apply Bs.
-    apply SRs.
-
-    invclr BP.
-    rename H3 into BP.
-    rename H4 into Bs.
-    rename H5 into Bt.
-    rename H2 into EQ.
-    rewrite <- interleave_length_split in EQ.
-    replace s_size with (length ss) in *; try omega.
-    replace t_size with (length ts) in *; try omega.
-    apply IHIR in SRt; eauto.
-    apply ListIndexR_interleave_odds; eauto.
-    symmetry. eapply BraunR_SequenceR. apply Bs.
-    apply SRs.
   Qed.
 
   Theorem MakeArrayLinearR_correct :
---
+++ new/sequence.v
@@ -0,0 +1,82 @@
+Require Import braun log insert util index list.
+Require Import Arith Arith.Even Arith.Div2 List.
+Require Import Program.
+Require Import Omega.
+
+Section sequence.
+  Variable A : Set.
+
+  Inductive SequenceR : bin_tree A -> list A -> Prop :=
+    | SR_mt :
+        SequenceR (bt_mt A) nil
+    | SR_node :
+        forall x s t ss ts,
+          SequenceR s ss ->
+          SequenceR t ts ->
+          SequenceR (bt_node x s t) (x::interleave A ss ts).
+  Hint Constructors SequenceR.
+
+  Lemma BraunR_SequenceR :
+    forall b n,
+      Braun b n ->
+      forall l,
+        SequenceR b l ->
+        n = (length l).
+  Proof.
+    intros b n B.
+    induction B; intros l SR; invclr SR.
+    auto.
+
+    rename H into BP.
+    rename H4 into SRs.
+    rename H5 into SRt.
+    apply IHB1 in SRs.
+    apply IHB2 in SRt.
+    subst.
+    rewrite interleave_length_split.
+    simpl.
+    omega.
+  Qed.
+
+  Theorem SequenceR_IndexR :
+    forall b i x,
+      IndexR A b i x ->
+      forall xs,
+        Braun b (length xs) ->
+        SequenceR b xs ->
+        ListIndexR A xs i x.
+  Proof.
+    intros b i x IR.
+    induction IR; intros xs BP SR; invclr SR; eauto;
+    rename H3 into SRs; rename H4 into SRt.
+
+    eapply LIR_zero.
+
+    invclr BP.
+    rename H3 into BP.
+    rename H4 into Bs.
+    rename H5 into Bt.
+    rename H2 into EQ.
+    rewrite <- interleave_length_split in EQ.
+    replace s_size with (length ss) in *; try omega.
+    replace t_size with (length ts) in *; try omega.
+    apply IHIR in SRs; eauto.
+    apply ListIndexR_interleave_evens; eauto.
+    symmetry. eapply BraunR_SequenceR. apply Bs.
+    apply SRs.
+
+    invclr BP.
+    rename H3 into BP.
+    rename H4 into Bs.
+    rename H5 into Bt.
+    rename H2 into EQ.
+    rewrite <- interleave_length_split in EQ.
+    replace s_size with (length ss) in *; try omega.
+    replace t_size with (length ts) in *; try omega.
+    apply IHIR in SRt; eauto.
+    apply ListIndexR_interleave_odds; eauto.
+    symmetry. eapply BraunR_SequenceR. apply Bs.
+    apply SRs.
+  Qed.
+
+End sequence.

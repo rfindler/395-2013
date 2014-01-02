0b75c06acb1de1913e3a931ad8ce3f863c687bc4
Robby Findler <robby@racket-lang.org>
2014-01-01 20:37:04 -0600

    added some timing stuff for the unravel version of make-array

--- rkt/make-array.rkt
+++ rkt/make-array.rkt
@@ -1,5 +1,7 @@
 #lang racket
-(require "braun.rkt")
+(require "braun.rkt" "log.rkt")
+
+(module+ slideshow (require slideshow plot "pict.rkt"))
 
 (define (insert x bt)
   (match bt
@@ -63,8 +65,6 @@
 
 
 (module+ slideshow
-  (require slideshow "pict.rkt")
-  
   (slide 
    (apply 
     para
@@ -72,3 +72,83 @@
     (for/list ([i (in-range 32)])
       (tree->pict (naive-make-array (build-list i values))
                   #f)))))
+
+(define (unravel+time l)
+  (cond
+    [(null? l) (values '() '() 0)]
+    [else
+     (define-values (odds evens time) (unravel+time (cdr l)))
+     (values (cons (car l) evens) odds (+ time 1))]))
+
+(define (td-make-array+time l)
+  (cond
+    [(null? l) (values #f 0)]
+    [else 
+     (define-values (odds evens time1) (unravel+time (cdr l)))
+     (define-values (left time2) (td-make-array+time odds))
+     (define-values (right time3) (td-make-array+time evens))
+     (values (mknode #:val (car l) left right)
+             (+ time1 time2 time3 1))]))
+
+(define (td-make-array l)
+  (define-values (bt time) (td-make-array+time l))
+  bt)
+
+(module+ test 
+  (printf "testing td-make-array against naive-make-array\n")
+  (let ()
+    (define l '())
+    (for ([i (in-range 1024)])
+      (unless (equal? (td-make-array l)
+                      (naive-make-array l))
+        (error 'td-make-array "disagrees with naive-make-array for ~s" l))
+      (set! l (cons i l)))))
+
+(define (td-make-array-time l)
+  (define-values (bt time) (td-make-array+time l))
+  time)
+
+(define (td-make-array-time2 len)
+  (cond
+    [(zero? len) 0]
+    [else
+     (+ (td-make-array-time2 (div2 len))
+        (td-make-array-time2 (div2 (- len 1)))
+        len)]))
+
+(module+ test 
+  (printf "testing td-make-array's time\n")
+  (let ()
+    (define l '())
+    (for ([i (in-range 1024)])
+      (define real-time (td-make-array-time l))
+      (define hoped-for-time (td-make-array-time2 i))
+      (unless (equal? real-time hoped-for-time)
+        (error 'td-make-array-time
+               "disagrees with naive-make-array for ~s: real ~s hoped-for ~s" 
+               i
+               real-time
+               hoped-for-time))
+      (set! l (cons i l)))))
+
+(module+ slideshow
+  (require (only-in math fllog2))
+  (define (plot-td-make-array-time upper-bound)
+    (plot-pict
+     #:x-label "n"
+     #:y-label "make_array_time(n) & n*cl_log(n)"
+     (list
+      (lines 
+       (for/list ([n (in-range upper-bound)])
+         (vector n (td-make-array-time2 n))))
+      (lines 
+       #:color "red"
+       (for/list ([n (in-range upper-bound)])
+         (vector n (* n (cl_log n))))))))
+  
+  (slide 
+   (scale-to-fit
+    (hc-append 40
+               (plot-td-make-array-time (expt 2 11))
+               (plot-td-make-array-time 10))
+    client-w client-h)))

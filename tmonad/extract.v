f9320291dd7e9e814aee84386b26e57c909e5d8f
Jay McCarthy <jay.mccarthy@gmail.com>
2014-01-04 22:59:44 -0700

    adding to extract

--- tmonad/extract.v
+++ tmonad/extract.v
@@ -1,5 +1,5 @@
 Require Import Arith.Div2 Arith.Even.
-Require Import Braun.tmonad.insert Braun.tmonad.copy Braun.tmonad.size.
+Require Import Braun.tmonad.insert Braun.tmonad.copy Braun.tmonad.size Braun.tmonad.size2.
 
 Extract Inductive bool => "bool" [ "false" "true" ].
 Extract Inductive sumbool => "bool" [ "false" "true" ].
@@ -14,4 +14,4 @@ Extract Constant mult => "fun x y -> x * y".
 Extract Constant div2 => "fun a -> a / 2".
 Extract Constant even_odd_dec => "fun a -> (a mod 2) != 0".
 
-Extraction "tmonad.ml" insert.insert copy.copy size.size_linear.
+Extraction "tmonad.ml" insert.insert copy.copy size.size_linear size2.size.

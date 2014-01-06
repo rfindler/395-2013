130dce7a9d76a67ad3690d67338ba6e318d3f2ac
Robby Findler <robby@racket-lang.org>
2014-01-05 19:29:16 -0600

    tell the extraction process to inline the monad operations

--- tmonad/extract.v
+++ tmonad/extract.v
@@ -1,5 +1,6 @@
 Require Import Arith.Div2 Arith.Even.
 Require Import Braun.tmonad.insert Braun.tmonad.copy Braun.tmonad.size Braun.tmonad.size2.
+Require Import Braun.tmonad.monad.
 
 Extract Inductive bool => "bool" [ "false" "true" ].
 Extract Inductive sumbool => "bool" [ "false" "true" ].
@@ -14,5 +15,7 @@ Extract Constant minus => "fun x y -> x - y".
 
 Extract Constant div2 => "fun a -> a / 2".
 Extract Constant even_odd_dec => "fun a -> (a mod 2) != 0".
+
+Extraction Inline ret bind inc.
 
 Extraction "tmonad.ml" insert.insert copy.copy size.size_linear size2.size.

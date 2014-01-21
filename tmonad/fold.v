982ae0e7fb8629c0380220fc680d18d93cdd62b6
Max New <maxsnew@gmail.com>
2014-01-20 23:38:46 -0600

    Fix fold

--- tmonad/fold.v
+++ tmonad/fold.v
@@ -4,42 +4,37 @@ Require Import Braun.tmonad.monad.
 Require Import Program List.
 Require Import Omega.
 
-Program Fixpoint foldr (A:Set) (B:Set) (PF:B -> (list A) -> nat -> Prop)
-  (f:forall (a:A) (b:B),
-       C B 
-         (fun b' b'n => 
-            forall al bn,
-              PF b al bn ->
-              PF b' (a :: al) (bn+b'n)))
-  (b:B) (PFb0:PF b nil 0) (al:list A) : 
-  {! b' !:! B !<! c !>! PF b' al c !} :=
-  match al with
-    | nil =>
-      <== b
-    | cons a al =>
-      ++ ; 
-      b'  <- (f a b) ;
-      b'' <- foldr A B (fun b al n => exists b'n, PF b (cons a al) (n+b'n)) f b' _ al ;
-      <== b''
-  end.
+(* Note: adds a ++ to every recursive call, but not for the base case. Should the base case have a ++ too? *)
+Section foldr.
+  Variables A B : Set.
+  Variable P : B -> (list A) -> nat -> Prop.
+  Variable f : forall (x:A) (acc:B),
+                 {! acc' !:! B !<! c !>!
+                    forall xs accC,
+                      P acc         xs           accC -> 
+                      P acc' (x :: xs) (c + accC + 1) !}.
+  Variable base : B.
+  Variable PFbase : P base nil 0.
+  Program Fixpoint foldr (l : list A) : {! b' !:! B !<! c !>! P b' l c !} :=
+    match l with
+      | nil => 
+        <== base
+      | cons x xs =>
+        ++ ;
+        acc <- foldr xs;
+        out <- f x acc;
+        <== out
+    end.
 
-Next Obligation.
-  rename H into IH.
-  rename x into a'.
-  rename x0 into b''.
-  apply IH in PFb0.
-  exists xm.
-  intros al bn.
-  intros [b'n PFb''].
-  exists b'n.
+  Next Obligation.
+    (* I suck at coq :( *)
+    assert ((xn0 + (xn + 0) + 1) = xn + xn0 + 1) as Dumb; [omega | rewrite Dumb; clear Dumb].
+    auto.
+  Defined.
 
-  (* XXX broken *)
-  admit.
-Qed.
-  
-Admit Obligations.
+End foldr.
 
-Program Fixpoint sum (l:list nat)
+Program Definition sum (l:list nat)
 : {! n !:! nat !<! c !>! 
      (forall x, In x l -> x <= n)
      /\ c = length l !} 
@@ -55,7 +50,7 @@ Next Obligation.
   rename l into al.
   rename x into sum.
   rename x0 into c.
-  exists 1.
+  exists 0.
   intros al' bn PF.
   destruct PF as [PF EQ]. subst.
   split; try omega.
@@ -67,5 +62,21 @@ Qed.
 Next Obligation.
   tauto.
 Qed.
-  
+
 Recursive Extraction sum.
+
+Program Definition list_id (A : Set) (l : list A) : {! l' !:! list A !<! c !>!
+                                                       l' = l !} :=
+  foldr A
+        (list A)
+        (fun xs ys n => xs = ys)
+        cons
+        nil 
+        _
+        l.
+Next Obligation.
+  exists 0.
+  intros ys _ H.
+  rewrite H.
+  auto.
+Qed.

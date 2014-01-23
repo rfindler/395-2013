11b9938eaf76c53545784dcc1bc7fe53d3d28707
Robby Findler <robby@racket-lang.org>
2014-01-23 14:17:12 -0600

    tweak big_oh definition to use le, not lt
    also prove transitivity and a simple result.

--- common/big_oh.v
+++ common/big_oh.v
@@ -1,8 +1,42 @@
-Set Implicit Arguments.
+Require Import Braun.common.log Braun.common.le_util.
+Require Import Arith.
 
-(* definition taken from Wikipedia *)
+(* definition taken from Wikipedia, except that
+   Wikipedia has n0 < n, not n0 <= n *)
 Definition big_oh f g := 
   exists n0 m, 
     forall n, 
-      n0 < n -> 
+      n0 <= n -> 
       f(n) <= m * g(n).
+
+Lemma big_oh_trans :
+  forall f g h,
+    big_oh f g ->
+    big_oh g h ->
+    big_oh f h.
+Proof.
+  intros f g h [FGn [FGm FGP]] [GHn [GHm GHP]].
+  exists (FGn+GHn).
+  exists (FGm*GHm).
+  intros n LE.
+  apply (le_trans (f n)
+                  (FGm * g n)
+                  (FGm * GHm * h n)).
+  apply FGP.
+  omega.
+  rewrite <- mult_assoc.
+  apply le_mult_right.
+  apply GHP.
+  omega.
+Qed.
+
+Lemma big_oh_fl_log_plus_1 : big_oh (fun n => (fl_log n + 1)) fl_log.
+Proof.
+  exists 1.
+  exists 2.
+  intros n LE.
+  destruct n; intuition.
+  destruct n; intuition.
+  rewrite <- fl_log_div2.
+  omega.
+Qed.

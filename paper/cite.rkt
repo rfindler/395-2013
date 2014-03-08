f0c68547d7e1641fccf7174757c74ba8567d95ae
Robby Findler <robby@racket-lang.org>
2014-03-08 09:13:13 -0600

    build up a bib based on the known papers to cite

--- paper/cite.rkt
+++ paper/cite.rkt
@@ -1,11 +1,25 @@
 #lang racket/base
 
 (require scriblib/autobib)
-(provide (all-defined-out))
+(provide (except-out (all-defined-out) fpca jfp types icfem waaapl))
 (define-cite ~cite citet generate-bibliography)
 
 (define fpca
   "Proc. International Conference on Functional Programming Languages And Computer Architecture")
+(define jfp
+  "Journal of Functional Programming")
+(define types
+  "Proc. Workshop of the Working Group Types")
+(define icfem
+  "Proc. International Conference on Formal Engineering Methods and Software Engineering")
+(define waaapl
+  "Proc. Workshop on Algorithmic Aspects of Advanced Programming Languages")
+(define esop
+  "Proc. European Symposium on Programming")
+(define tphols
+  "Proc. Theorem Proving in Higher Order Logics")
+(define popl
+  "Proc. Symposium on Principles of Programming Languages")
 
 ;; need to check on this cite; just got it from Chris's paper
 (define Braun
@@ -23,3 +37,63 @@
    #:author "Mads Rosendahl"
    #:location (proceedings-location fpca)
    #:date 1989))
+
+(define three-algorithms-on-braun-trees
+  (make-bib
+   #:title "Three Algorithms on Braun Trees"
+   #:author "Chris Okasaki"
+   #:location (journal-location jfp #:number 6 #:volume 7)
+   #:date 1997))
+
+(define a-machine-checked-proof-of-the-average-case-complexity-of-quicksort-in-coq
+  (make-bib
+   #:title "A Machine-Checked Proof of the Average-Case Complexity of Quicksort in Coq"
+   #:author (authors "Eelis van der Weegen" "James McKinna")
+   #:location (proceedings-location types)
+   #:date 2008))
+
+(define characteristic-formulae-for-mechanized-program-verification
+  (make-bib
+   #:title "Characteristic Formulae for Mechanized Program Verification"
+   #:author "Arthur Charguéraud"
+   #:location (dissertation-location
+               #:institution "Université Paris Diderot (Paris 7)"
+               #:degree "PhD")
+   #:date 2010))
+
+(define correct-by-construction-model-transformations
+  (make-bib
+   #:title (string-append "Correct-by-Construction Model Transformations"
+                          " from Partially Ordered Specifications in Coq")
+   #:author (authors "Iman Poernomo" "Jeffrey Terrell")
+   #:location (proceedings-location icfem)
+   #:date 2010))
+
+(define dependently-typed-datastructures
+  (make-bib
+   #:title "Dependently Typed Data Structures"
+   #:author "Hongwei Xi"
+   #:location (proceedings-location waaapl)
+   #:date 1999))
+
+(define functors-for-proofs-and-programs
+  (make-bib
+   #:title "Functors for Proofs and Programs"
+   #:author (authors "Jean-Christophe Filliâtre"
+                     "Pierre Letouzey")
+   #:location (proceedings-location esop)
+   #:date 2004))
+
+(define hoare-logic-state-monad
+  (make-bib
+   #:title "A Hoare Logic for the State Monad"
+   #:author "Wouter Swierstra"
+   #:location (proceedings-location tphols)
+   #:date 2009))
+   
+(define lightweight-semiformal-time-complexity-analysis-for-purely-functional-data-structures
+  (make-bib
+   #:title "Lightweight Semiformal Time Complexity Analysis for Purely Functional Data Structures"
+   #:author "Nils Anders Danielsson"
+   #:location (proceedings-location popl)
+   #:date 2008))
--- paper/paper.scrbl
+++ paper/paper.scrbl
@@ -78,6 +78,24 @@ Other things?
 
 @section{Related Work}
 
+@citet[a-machine-checked-proof-of-the-average-case-complexity-of-quicksort-in-coq]
+
+@citet[automatic-complexity-analysis]
+
+@citet[characteristic-formulae-for-mechanized-program-verification]
+
+@citet[correct-by-construction-model-transformations]
+
+@citet[dependently-typed-datastructures]
+
+@citet[functors-for-proofs-and-programs]
+
+@citet[hoare-logic-state-monad]
+
+@citet[lightweight-semiformal-time-complexity-analysis-for-purely-functional-data-structures]
+
+@citet[three-algorithms-on-braun-trees]
+
 @section{Conclusion}
 
 @generate-bibliography[]

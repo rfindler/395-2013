1a766401bd637ec9e4b3bde136f82872caa500e6
Robby Findler <robby@racket-lang.org>
2014-06-20 22:56:41 -0500

    generalize the store monad used in the queue example to make it more plausibly extract as reference cells
    The current extraction still has all of the monadic
    plumbing, tho.

--- queue/queue.v
+++ queue/queue.v
@@ -3,10 +3,13 @@ Require Import Omega.
 Require Import List.
 Require Import Braun.common.util.
 
-(* this is the state monad, fixed to just a single
-   store element that is a pair of two lists of 
+(* this is the type of things in the store *)
+Definition Q := (list nat * list nat)%type.
+
+(* this is the state type, a finite map from
+   addresses to pairs of two lists of 
    integers, ie our queue's internal state *)
-Definition ST := (list nat * list nat)%type.
+Definition ST := ((nat -> Q) * nat)%type.
 
 Definition S (A:Set) := ST -> (A * ST).
 
@@ -19,59 +22,81 @@ Definition bind {A : Set} {B : Set} : (S A) -> (A -> S B) -> S B :=
         | (a, st') => ((f a) st') 
       end.
 
-Definition get : S ST := fun st => (st,st).
-Definition set : ST -> S () := fun new => fun old => ((),new).
+Definition get : nat -> S Q := 
+  fun addr st => ((fst st) addr,st).
+Definition set : nat -> Q -> S () := 
+  fun addr new_q => 
+    fun old_st => 
+      ((),
+       (fun addr' => if eq_nat_dec addr addr' 
+                     then new_q 
+                     else (fst old_st) addr',
+        snd old_st)).
+Definition alloc : S nat :=
+  fun st =>
+    (snd st,(fst st,snd st+1)).
+
+(* all allocated values are initialized as a pair of empty lists *)
+Definition init_st : ST := (fun n => (nil,nil),0).
 
 Definition run {A : Set} : S A -> A := 
-  fun v => match v (nil,nil) with
+  fun v => match v init_st with
              | (v,st) => v
            end.
 
-(* returns the queue in list order after the end 
-   of the monadic computation, in addition to 
-   returning the result of the monadic computation *)
-Definition run_s {A : Set} : S A -> (A * list nat) := 
-  fun v => match v (nil,nil) with
-             | (v,(l1,l2)) => (v, l2 ++ rev' l1)
-           end.
+(* given a computation and some interesting addresses, 
+   runs the computation and extracts the interesting store values,
+   returning them as lists in the order the queue would return them. *)
+Definition run_s {A : Set} : S A -> (list nat) -> (A * list (list nat)) := 
+  fun v addrs => match v init_st with
+                   | (v,(st,addr)) => 
+                     (v, map (fun addr => match st addr with
+                                            | (l1,l2) => l2 ++ rev' l1
+                                          end)
+                             addrs)
+                 end.
 
-Definition enq : nat -> S () := 
-  fun n => 
-    bind get
+Definition enq : nat -> nat -> S () := 
+  fun addr n => 
+    bind (get addr)
          (fun q =>
            match q with
-             | (front,back) => set (cons n front,back)
+             | (front,back) => set addr (cons n front,back)
            end).
 
-Definition deq : S (option nat) :=
-  bind get
-       (fun q => 
-          match q with
-            | (nil,nil) => ret None
-            | (front,cons back more) => 
-              bind (set (front,more))
-                   (fun _ => ret (Some back))
-            | (cons front more, nil) =>
-              match rev' (cons front more) with
-                | nil => ret None (* can't happen *)
-                | (cons back more') =>
-                  bind (set (nil,more'))
-                       (fun _ => ret (Some back))
-                           end 
-          end).
+Definition deq : nat -> S (option nat) :=
+  fun addr =>
+    bind (get addr)
+         (fun q => 
+            match q with
+              | (nil,nil) => ret None
+              | (front,cons back more) => 
+                bind (set addr (front,more))
+                     (fun _ => ret (Some back))
+              | (cons front more, nil) =>
+                match rev' (cons front more) with
+                  | nil => ret None (* can't happen *)
+                  | (cons back more') =>
+                    bind (set addr (nil,more'))
+                         (fun _ => ret (Some back))
+                end 
+            end).
 
-Example ex1 : run (bind (enq 1) (fun _ => deq)) = run (ret (Some 1)).
+Example ex1 : run (bind alloc (fun addr => (bind (enq addr 1) (fun _ => (deq addr)))))
+              = run (ret (Some 1)).
 Proof.
   compute.
   reflexivity.
 Qed.
 
-Example ex2 : run (bind (enq 1) 
-                        (fun _ =>
-                           bind (enq 2)
+Example ex2 : run (bind alloc
+                        (fun addr =>
+                           bind (enq addr 1) 
                                 (fun _ =>
-                                   bind deq 
-                                        (fun _ => deq))))
+                                   bind (enq addr 2)
+                                        (fun _ =>
+                                           bind (deq addr)
+                                                (fun _ => (deq addr))))))
               = run (ret (Some 2)).
 Proof.
   compute.
@@ -79,10 +104,11 @@ Proof.
 Qed.
 
 Theorem enq_correct : 
-  forall l1 l2 n,
-    run_s (bind (set (l1,l2))
-                (fun _ => (enq n)))
-         = ((), l2 ++ rev' l1 ++ [n]).
+  forall l1 l2 n addr,
+    run_s (bind (set addr (l1,l2))
+                (fun _ => (enq addr n)))
+          [addr]
+         = ((), [l2 ++ rev' l1 ++ [n]]).
 Proof.
   unfold enq.
   unfold bind.
@@ -90,7 +116,10 @@ Proof.
   unfold get.
   unfold ret.
   unfold run_s.
-  intros l1 l2 n.
+  intros l1 l2 n addr.
+  simpl.
+  dispatch_if X1 X1';[|intuition].
+  dispatch_if X2 X2';[|intuition].
   replace (rev' (n::l1)) with (rev (n::l1));
     [| unfold rev' ; rewrite rev_alt; auto].
   simpl.
@@ -111,15 +140,16 @@ Lemma nonempty_rev_nonempty : forall (x:nat) xs, [] = rev' (x :: xs) -> False.
 Qed.
 
 Theorem deq_correct : 
-  forall l1 l2,
-    run_s (bind (set (l1,l2))
-                (fun _ => deq))
+  forall l1 l2 addr,
+    run_s (bind (set addr (l1,l2))
+                (fun _ => deq addr))
+          [addr]
     = match (l2 ++ rev' l1) with
-        | [] => (None, nil)
-        | (x :: xs) => (Some x, xs)
+        | [] => (None, [nil])
+        | (x :: xs) => (Some x, [xs])
       end.
 Proof.
-  intros l1 l2.
+  intros l1 l2 addr.
   remember (app l1 (rev' l2)) as STATE.
   destruct STATE.
 
@@ -136,13 +166,18 @@ Proof.
   auto.
   subst l2.
   compute.
-  reflexivity.
+  dispatch_if X X';intuition.
+  dispatch_if Y Y';intuition.
 
   (* queue nonempty *)
 
   destruct l2.
 
-  destruct l1; auto.
+  destruct l1.
+  simpl.
+  compute.
+  dispatch_if X X';intuition.
+  dispatch_if Y Y';intuition.
 
   (* back of queue is empty *)
   unfold deq.
@@ -152,12 +187,15 @@ Proof.
   unfold ret.
   unfold run_s.
   simpl.
+  dispatch_if X X';[clear X|intuition].
 
   remember (rev' (n0::l1)) as LST.
   destruct LST.
   assert False;[|intuition].
   apply (nonempty_rev_nonempty n0 l1).
   auto.
+
+  dispatch_if X X';[clear X|intuition].
 
   unfold rev'.
   simpl.
@@ -173,5 +211,7 @@ Proof.
   unfold run_s.
   simpl.
 
-  destruct l1; auto.
+  dispatch_if X X';[clear X|intuition].
+
+  destruct l1; (dispatch_if X X';[clear X|intuition]); auto.
 Qed.

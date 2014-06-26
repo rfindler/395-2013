26d6bcefc59cfd1af3e0e64def1ccc4df64ac9ad
Robby Findler <robby@racket-lang.org>
2014-06-26 07:28:33 -0500

    change time to be only in the postcondition, and thus to be a delta of how long it takes to get from the pre state to the post state
    also, specify that get and set take 0 steps

--- monad/smonad.v
+++ monad/smonad.v
@@ -11,24 +11,24 @@ Definition Q := (list nat * list nat)%type.
 (* integers, ie our queue's internal state   *)
 Definition ST := ((nat -> Q) * nat)%type.
 
-Definition CS 
+Definition CS
            (A:Set)
-           (Pre : ST -> nat -> Prop) 
+           (Pre : ST -> Prop)
            (Post: A -> ST -> ST -> nat -> Prop) : Set :=
   forall st:ST,
-    {(a,st') : A * ST | forall pren, 
-                          Pre st pren -> 
-                          exists an,
-                            Post a st st' (pren + an)}.
+    {(a,st') : A * ST | Pre st ->
+                        exists an,
+                          Post a st st' an}.
 Hint Unfold CS.
 
 Definition ret (A:Set) (a:A)
-: CS A (fun st time => True)
-     (fun a' st st' time => st=st' /\ a=a').
+: CS A
+     (fun st => True)
+     (fun a' st st' time => st=st' /\ a=a' /\ time = 0).
 Proof.
   intros st.
   exists (a,st).
-  intros pren pre_st0.
+  intros pren.
   exists 0.
   auto.
 Defined.
@@ -36,15 +36,18 @@ Defined.
 (* START: bind *)
 Definition bind 
            (A:Set) (B:Set)
-           (A_Pre:ST -> nat -> Prop)
+           (A_Pre:ST -> Prop)
            (A_Post:A -> ST -> ST -> nat -> Prop)
-           (B_Pre:A -> ST -> nat -> Prop)
+           (B_Pre:A -> ST -> Prop)
            (B_Post:A -> B -> ST -> ST -> nat -> Prop)
            (am:CS A A_Pre A_Post)
            (bf:forall (a:A), CS B (B_Pre a) (B_Post a))
 : CS B 
-     (fun st n => A_Pre st n /\ forall a st' an, A_Post a st st' an -> B_Pre a st' an)
-     (fun b st0 st2 n => exists a st1 bn, A_Post a st0 st1 n /\ B_Post a b st1 st2 (n+bn)).
+     (fun st => A_Pre st /\ forall a st' an, A_Post a st st' an -> B_Pre a st')
+     (fun b st0 st2 n => exists a st1 an bn,
+                           A_Post a st0 st1 an /\
+                           B_Post a b st1 st2 bn /\
+                           n = an+bn).
 (* STOP: bind *)
 Proof.
   intros st0.
@@ -55,17 +58,18 @@ Proof.
   exists (b,st2).
 
   (* proof obligations part *)
-  intros pren [A_PRE A_POST_TO_B_PRE].
-  destruct (A_PROP pren A_PRE) as [an INT]; clear A_PROP A_PRE.
-  destruct (B_PROP (pren+an) (A_POST_TO_B_PRE a st1 (pren + an) INT)) as [bn POST].
-  exists an a st1 bn.
-  split;assumption.
+  intros [A_PRE A_POST_TO_B_PRE].
+  destruct (A_PROP A_PRE) as [an INT]; clear A_PROP A_PRE.
+  destruct (B_PROP (A_POST_TO_B_PRE a st1 an INT)) as [bn POST].
+  exists (an+bn) a st1.
+  exists an bn.
+  repeat split; try assumption.
 Defined.
 
 Definition inc 
            (A:Set)
            k
-           (Pre:ST -> nat -> Prop)
+           (Pre:ST -> Prop)
            (Post:A -> ST -> ST -> nat -> Prop)
            (C : CS A Pre 
                    (fun a st st' time =>
@@ -77,32 +81,33 @@ Proof.
   intros st.
   destruct (C st) as [[a st'] P].
   exists (a,st').
-  intros pren PRE.
-  destruct (P pren PRE) as [postn POST].
+  intros PRE.
+  destruct (P PRE) as [postn POST].
   exists (postn+k).
   apply POST.
-  rewrite plus_assoc.
   reflexivity.
 Defined.
 
 Definition get : 
   forall addr, 
     CS Q 
-       (fun st n => True)
-       (fun q st_pre st_post n => ((fst st_post) addr) = q /\ st_pre = st_post).
+       (fun st => True)
+       (fun q st_pre st_post time =>
+          time = 0 /\ ((fst st_post) addr) = q /\ st_pre = st_post).
 Proof.
   intros addr [fm high_addr].
   exists (fm addr,(fm,high_addr)).
-  intros gt _.
-  exists gt.
-  split; reflexivity.
+  intros _.
+  exists 0.
+  repeat split; reflexivity.
 Defined.
 
 Definition set:
   forall addr nv,
     CS ()
-       (fun st n => True)
-       (fun q st_pre st_post n =>
+       (fun st => True)
+       (fun q st_pre st_post time =>
+          time = 0 /\
           (snd st_pre) = (snd st_post) /\
           ((fst st_post) addr) = nv /\ 
           forall addr', 
@@ -111,8 +116,8 @@ Definition set:
 Proof.
   intros addr nv [fm high_addr].
   exists (tt,(fun addr' => if (eq_nat_dec addr addr') then nv else (fm addr'),high_addr)).
-  intros t _.
-  exists t.
+  intros _.
+  exists 0.
   simpl.
   repeat split.
   destruct (eq_nat_dec addr addr);intuition.
@@ -122,16 +127,17 @@ Defined.
 
 Definition alloc : 
   CS nat 
-     (fun st n => True)
-     (fun res st_pre st_post n =>
+     (fun st => True)
+     (fun res st_pre st_post time =>
+        time = 0 /\
         res = (snd st_pre) /\
         (snd st_post) = (snd st_pre)+1).
 Proof.
   intros [fm high_addr].
   exists (high_addr,(fm,high_addr+1)).
-  intros t _.
-  exists t.
-  split; reflexivity.
+  intros _.
+  exists 0.
+  repeat split; reflexivity.
 Defined.
 
 (* all allocated values are initialized as a pair of empty lists *)

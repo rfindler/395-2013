8af83cc2cd419b5596827bb2bc1ae308fe71637a
Jay McCarthy <jay.mccarthy@gmail.com>
2015-02-23 22:12:05 -0600

    slowly...

--- clrs/rbt_balance_gen.rkt
+++ clrs/rbt_balance_gen.rkt
@@ -6,8 +6,8 @@
 (define-syntax (Fixpoint* stx)
   (define (expand-match* stx)
     (syntax-parse stx
-      [(_ (expr ...)
-          [(pat ...) body]
+      [(_ #:else else:expr
+          [{id pat} ... body]
           ...)
        (syntax/loc stx
          (<== 1))]))
@@ -23,15 +23,13 @@
  rbt_balance #:implicit @A{Set}
  @tl{CTree A} @tc{Color} @tv{A} @tr{CTree A}
  #:returns @{CTree A}
- (match* (tl tv tr)
-   [{tc BLACK} ((CT_node (CT_node a RED xV b) RED yV c) zV d)
-    (CT_node (CT_node a BLACK xV b) RED yV (CT_node c BLACK zV d))]
-   [{tc BLACK} ((CT_node a RED xV (CT_node b RED yV c)) zV d)
-    (CT_node (CT_node a BLACK xV b) RED yV (CT_node c BLACK zV d))]
+ (match* #:else (CT_node tl tc tv tr)
+   [{tc BLACK} {tl (CT_node (CT_node a RED xV b) RED yV c)}
+    (CT_node (CT_node a BLACK xV b) RED yV (CT_node c BLACK tv tr))]
+   [{tc BLACK} {tl (CT_node a RED xV (CT_node b RED yV c))}
+    (CT_node (CT_node a BLACK xV b) RED yV (CT_node c BLACK tv tr))]
    
-   [{tc BLACK} (a xV (CT_node (CT_node b RED yV c) RED zV d))
-    (CT_node (CT_node a BLACK xV b) RED yV (CT_node c BLACK zV d))]
-   [{tc BLACK} (a xV (CT_node b RED yV (CT_node c RED zV d)))
-    (CT_node (CT_node a BLACK xV b) RED yV (CT_node c BLACK zV d))]
-   
-   [(CT_node tl tc tv tr)]))
+   [{tc BLACK} {tr (CT_node (CT_node b RED yV c) RED zV d)}
+    (CT_node (CT_node tl BLACK tv b) RED yV (CT_node c BLACK zV d))]
+   [{tc BLACK} {tr (CT_node b RED yV (CT_node c RED zV d))}
+    (CT_node (CT_node tl BLACK tv b) RED yV (CT_node c BLACK zV d))]))

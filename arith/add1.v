a9dcb744528d1ed9960934b07e8d2a516a097fb9
Robby Findler <robby@racket-lang.org>
2015-03-01 14:52:37 -0600

    finish add1

--- arith/add1.v
+++ arith/add1.v
@@ -49,24 +49,23 @@ Next Obligation.
   apply (not_even_and_odd (S wildcard')); auto.
 Qed.
 
-(*
 Program Fixpoint add1_time2 (n:nat) {measure n} :=
   match n with
     | 0 => 0
     | S _ => if (even_odd_dec n)
-             then (add1_time2 (div2 n)) + 1
-             else 1
+             then 1
+             else (add1_time2 (div2 n)) + 1
   end.
 
 Lemma add1_time12 : big_oh add1_time add1_time2.
   exists 1.
-  exists 50.
+  exists 14.
   intros n LE.
   destruct n; intuition.
   clear LE.
   apply (well_founded_induction
            lt_wf
-           (fun n => add1_time (S n) <= 50 * add1_time2 (S n))).
+           (fun n => add1_time (S n) <= 14 * add1_time2 (S n))).
   clear n; intros n IND.
   unfold_sub add1_time (add1_time (S n)); fold_sub add1_time.
   unfold_sub add1_time2 (add1_time2 (S n)); fold add1_time2.
@@ -74,14 +73,49 @@ Lemma add1_time12 : big_oh add1_time add1_time2.
   destruct n.
   simpl; omega.
   rewrite mult_plus_distr_l.
-  replace (50*1) with (38+12); [|omega].
-  rewrite plus_assoc.
-  apply le_plus_left.
-  apply le_add_right.
-  apply IND; auto.
+  apply plus_le_compat;[|omega].
+  apply IND; auto.  
 Qed.
 
-*)
+Theorem add1_log : big_oh add1_time cl_log.
+Proof.
+  apply (big_oh_trans add1_time add1_time2).
+  apply add1_time12.
+  
+  exists 0.
+  exists 1.
+  intros n _.
+  rewrite mult_1_l.
+  apply (well_founded_induction
+           lt_wf
+           (fun n => add1_time2 n <= cl_log n)).
+  clear n; intros n IND.
+  destruct n.
+  unfold_sub add1_time2 (add1_time2 0).
+  omega.
 
-Theorem add1_log : big_oh add1_time fl_log.
-Admitted.
+  unfold_sub add1_time2 (add1_time2 (S n)).
+  fold add1_time2.
+
+  dispatch_if COND1 COND1'.
+  rewrite <- fl_log_cl_log_relationship.
+  apply le_n_S.
+  omega.
+
+  destruct n.
+  simpl.
+  rewrite cl_log_div2'.
+  apply le_n_S.
+  omega.
+
+  apply (le_trans (add1_time2 (S (div2 n)) + 1)
+                  (cl_log (S (div2 n)) + 1)).
+  apply plus_le_compat;auto.
+  apply IND.
+  apply lt_n_S.
+  auto.
+
+  replace (cl_log (S (S n))) with (S (cl_log (div2 (S (S n)))));[|rewrite cl_log_div2';auto].
+  simpl div2.
+  omega.
+Qed.

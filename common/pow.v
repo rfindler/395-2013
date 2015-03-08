a005d0fab77f3172f1a156b86a749e10be77491b
Robby Findler <robby@racket-lang.org>
2015-03-08 18:47:19 -0500

    prove relationship between pow and log

--- common/pow.v
+++ common/pow.v
@@ -1,7 +1,7 @@
 Require Import Arith Arith.Even Arith.Div2 Omega.
 Require Import Coq.Logic.JMeq Coq.Program.Wf.
 Require Import Program.Syntax List.
-Require Import Braun.common.util.
+Require Import Braun.common.util Braun.common.log.
 
 Fixpoint pow n m :=
   match m with
@@ -39,3 +39,34 @@ Proof.
   simpl.  omega.
 Qed.
   
+Lemma pow_S_non_zero :
+  forall m n, 0 < pow (S m) n.
+ Proof.
+   intros m n.
+   induction n.
+   simpl.
+   omega.
+   simpl.
+   intuition.
+Qed.
+
+Lemma pow2_log :
+  forall n, cl_log (pow 2 n) = S n.
+Proof.
+  apply (well_founded_ind
+           lt_wf
+           (fun n => cl_log (pow 2 n) = (S n))).
+  intros n IND.
+  destruct n.
+  auto.
+  replace (pow 2 (S n)) with (2 * (pow 2 n));[|unfold pow;auto].
+  remember (pow 2 n) as m.
+  destruct m.
+  remember (pow_S_non_zero 1 n); intuition.
+  replace (2 * S m) with (m + 1 + m + 1);[|omega].
+  rewrite <- cl_log_even.
+  replace (m+1) with (S m);[|omega].
+  replace (cl_log (S m)+1) with (S (cl_log (S m)));[|omega].
+  assert (cl_log (S m) = S n);auto.
+  rewrite <- (IND n); auto.
+Qed.
